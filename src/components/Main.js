a2bfa20095e276c8e30a8a0a6c4f471d9062acb2
MirThomasson <mirthomasson@gmail.com>
2022-11-02 13:56:49 -0400

    discord link color change

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ export default function Main() {
     <main>
         <h1>Miranda Thomasson</h1>
         <p className='title'>Frontend Developer</p>
-        <a href='https://www.mirandathomasson.com/'>mirandathomasson.com</a>
+        <a href='https://www.mirandathomasson.com/' rel='noreferrer' target='_blank'>mirandathomasson.com</a>
         <div className='buttons'>
             <button><a href="mailto: mirthomasson@gmail.com" rel='noreferrer' target='_blank'><img src={envelope} alt="envelope" />Email</a></button>
             <button className='blue-button'><a href='https://www.linkedin.com/in/miranda-thomasson/' rel='noreferrer' target='_blank'><img src={linkedin} alt="linkedIn" />LinkedIn</a></button>
